03242993be2496e62fb95f532c5a1278d6ed7185
atom-7 <53164898+atom-7@users.noreply.github.com>
2019-11-28 18:50:51 +0800

    商品列表渲染+分页懒加载商品

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -5,62 +5,74 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      filterList:['综合','销量','价格'],// tab栏的文字
+      showIndex:0,// tab栏切换的inedx
+      pageIndex:0,// 获取数据的页码
+      productList:[],// 搜索获取回来的商品列表数据
+      total:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options);
+    const {query,cid} = options;
+    this.setData({
+      query,
+      cid
+    })
+    this.getSearchList();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 处理点击过滤的事件
+  handleIndex(e){
+ 
+    const {index} = e.currentTarget.dataset;
+    this.setData({
+      showIndex:index
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 获取搜索的列表
+  getSearchList(){
+    let max =Math.ceil(this.data.total/10);
+     if(this.data.pageIndex === max && this.data.pageIndex)return;
+       this.setData({
+         pageIndex:this.data.pageIndex+1
+       })
+       const {query,cid} = this.data;
+       console.log(this.data.pageIndex);
+      wx.request({
+        url: 'https://api.zbztb.cn/api/public/v1/goods/search',
+        data: {
+          query,
+          cid,
+          pagenum:this.data.pageIndex,
+          pagesize:10
+        },
+        success: (res) => {
+          console.log(res);
+          const{goods,total} = res.data.message;
+          const oldArr = this.data.productList;
+          this.setData({
+            ['productList']:[...oldArr,...goods],
+            total
+          })
+          console.log(this.data.productList);
+          
+        }
+      });
+        
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleGetBottom(){
+    this.getSearchList();
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  onReachBottom(){
+    console.log('onReachBottom');
+    this.getSearchList();
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onPullDownRefresh(){
+    console.log('onPullDownRefresh');
+    
   }
+
 })
