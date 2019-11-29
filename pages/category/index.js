cade30dab4681c70e12f05054b5c81bdc65bd687
atom-7 <53164898+atom-7@users.noreply.github.com>
2019-11-29 15:24:57 +0800

    优化分类页面数据存放,将大的数据放在data外,data只放要渲染的数据

--- pages/category/index.js
+++ pages/category/index.js
@@ -5,10 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    tabList:[],
+    leftMenu:[],
     currentIndex:0,
-    currentChild:[]
+    currentChild:[]// 右边商品列表
   },
+  cateData:[],// 总分类数据
   onLoad: function (options) {
     this.getTab()
   },
@@ -16,26 +17,33 @@ Page({
   onReady: function () {
 
   },
-  //获取tab栏数据
+  //!获取tab栏数据
   getTab(){
     wx.request({
       url: 'https://api.zbztb.cn/api/public/v1/categories',
      success: (res) => {
         const{message} = res.data;
-        console.log(message);
+        // 设置给全局数据,因为数据太多不能直接给data
+        this.cateData = message; 
+        //过滤出来名字,数据小
+        const leftNameList = message.map(item=>item.cat_name);
+       // 设置左边菜单的数据   当前右列表的数据
         this.setData({
-          tabList:message,
+          leftMenu:leftNameList,
           currentChild:message[0].children
         })
       }
     });
       
   },
-  // 当点击tab栏触发的函数
+  //! 当点击tab栏触发的函数
   handleActive(e){
-    const {child:currentChild,index:currentIndex} = e.currentTarget.dataset;
+    // 取出当前点击目标身上的自定义属性索引,
+    const {index} = e.currentTarget.dataset; // *这是给tab切换index激活样式用
+    // 根据索引取出当前的对应右边列表的数据
+    const currentChild = this.cateData[index].children;
     this.setData({
-      currentIndex,
+      currentIndex:index,
       currentChild
     })
   }
