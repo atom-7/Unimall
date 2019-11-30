54e63143f26f206eecfc6f4457a42f6704e0afa5
atom-7 <53164898+atom-7@users.noreply.github.com>
2019-11-30 17:20:26 +0800

    给商品列表的图片添加默认图片

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -1,23 +1,27 @@
 // pages/goods_list/goods_list.js
+import axios from '../../request/axios';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-      filterList:['综合','销量','价格'],// tab栏的文字
-      showIndex:0,// tab栏切换的inedx
-      pageIndex:0,// 获取数据的页码
-      productList:[],// 搜索获取回来的商品列表数据
-      total:0
-  },
+    filterList: ['综合', '销量', '价格'],// tab栏的文字
+    showIndex: 0,// tab栏切换的inedx
+    pageIndex: 0,// 获取数据的页码
+    productList: [],// 搜索获取回来的商品列表数据
+    total: 0,
+    // * 404图片路径
+    miss: 'https://www.webdesignerdepot.com/cdn-origin/uploads/2017/05/featured_404.jpg',
 
+  },
+  axios,
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     console.log(options);
-    const {query,cid} = options;
+    const { query, cid } = options;
     this.setData({
       query,
       cid
@@ -25,54 +29,65 @@ Page({
     this.getSearchList();
   },
   // 处理点击过滤的事件
-  handleIndex(e){
- 
-    const {index} = e.currentTarget.dataset;
+  handleIndex(e) {
+
+    const { index } = e.currentTarget.dataset;
     this.setData({
-      showIndex:index
+      showIndex: index
     })
   },
   // 获取搜索的列表
-  getSearchList(){
-    let max =Math.ceil(this.data.total/10);
-     if(this.data.pageIndex === max && this.data.pageIndex)return;
-       this.setData({
-         pageIndex:this.data.pageIndex+1
-       })
-       const {query,cid} = this.data;
-       console.log(this.data.pageIndex);
-      wx.request({
-        url: 'https://api.zbztb.cn/api/public/v1/goods/search',
-        data: {
-          query,
-          cid,
-          pagenum:this.data.pageIndex,
-          pagesize:10
-        },
-        success: (res) => {
-          console.log(res);
-          const{goods,total} = res.data.message;
-          const oldArr = this.data.productList;
-          this.setData({
-            ['productList']:[...oldArr,...goods],
-            total
-          })
-          console.log(this.data.productList);
-          
-        }
-      });
-        
+  getSearchList() {
+    //? 获取最大页数, 如果当前页码已经是最大页码,就return
+    let max = Math.ceil(this.data.total / 10);
+
+    // * 判断当前页数
+    if (this.data.pageIndex === max && this.data.pageIndex) return;
+
+    this.setData({  // *修改页码
+      pageIndex: this.data.pageIndex + 1
+    })
+    const { query, cid } = this.data;
+    console.log(this.data.pageIndex);
+    this.axios({
+      url: 'https://api.zbztb.cn/api/public/v1/goods/search',
+      data: {
+        query,
+        cid,
+        pagenum: this.data.pageIndex,
+        pagesize: 10
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        const { goods, total } = res.data.message;
+        const oldArr = this.data.productList;
+        this.setData({
+          ['productList']: [...oldArr, ...goods],
+          total
+        })
+        console.log(this.data.productList);
+      })
+    // wx.request({
+
+
+    //   success: (res) => {
+
+
+    //   }
+    // });
+
   },
-  handleGetBottom(){
+  handleGetBottom() {
     this.getSearchList();
   },
-  onReachBottom(){
+  onReachBottom() {
     console.log('onReachBottom');
     this.getSearchList();
   },
-  onPullDownRefresh(){
+  onPullDownRefresh() {
     console.log('onPullDownRefresh');
-    
+
   }
 
 })
