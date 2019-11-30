5b5b875bf3fd2b39e58f50aca90a8b9d81ec628e
atom-7 <53164898+atom-7@users.noreply.github.com>
2019-11-30 16:23:49 +0800

    封装的axios添加ajax加载提示

--- request/axios.js
+++ request/axios.js
@@ -6,19 +6,31 @@ function axios({ url: oldUrl, data, method, ...rest }) {
     } else {
         url = 'https://api.zbztb.cn/api/public/v1/' + oldUrl;
     };
-
+    let count = 0;
 
     return new Promise((resolv, reject) => {
+        count++;
+        wx.showLoading({
+            title: '加载中',
+            mask:true
+          })
         wx.request({
             url,
             data: data || "", // ? 没有data就空
             method: method || 'GET', //?不传 默认get
             ...rest,
             success: (result) => {
+               
                 resolv(result);
             },
             fail: (err) => {
                 reject(err);
+             },
+             complete(){ // *不管请求成功与否都会执行的函数
+                count--;
+                if(count===0){
+                    wx.hideLoading();
+                }
              }
         });
 
