5142a4b7f5730adc9faa4b5fc11351f71a8de3ae
atom-7 <53164898+atom-7@users.noreply.github.com>
2019-12-02 15:25:43 +0800

    给商品详情页轮播图的图片添加点击预览

--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -4,7 +4,8 @@ Page({
   data: {
     productDetail:{}
   },
-  axios,
+  imgList:[],// * 预览图片的路径列表
+  axios, // * 请求方法
   onLoad: function ({goods_id}) {
     
      this.axios({
@@ -13,12 +14,21 @@ Page({
         goods_id
        }
      }).then((res)=>{
-       console.log(res);
        const{message} = res.data;
+       // 这是给预览用的
+       this.imgList = message.pics.map(v=>v.pics_big);
+       // 这是设置商品的全部数据
        this.setData({
         productDetail:message
        })
 
      })
+  },
+  //* 处理点击图片预览事件
+  handlePreview(e){
+    wx.previewImage({
+      current: e.currentTarget.dataset.src, // 当前显示图片的http链接
+      urls: this.imgList // 需要预览的图片http链接列表
+    })
   }
 })
